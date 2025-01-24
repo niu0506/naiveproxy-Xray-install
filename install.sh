d922494b0e77d8c75877abd94daed8f8a711c4b2
niu0506 <xxhhxx2015.11.11@gmail.com>
2025-01-24 15:06:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,11 +10,10 @@ DOMAIN=${DOMAIN:-"example.com"}
 # go最新版本
 VERSION=$(curl -sSL "https://golang.org/VERSION?m=text")
 
-#caddy证书路径
+# caddy证书路径
 CERT_DIR="/var/lib/caddy/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$DOMAIN"
 
-#变量
-
+# 变量
 read -p "请输入邮箱（默认随机）: " EMAIL
 
 # 如果用户没有输入邮箱，则随机生成一个
@@ -37,12 +36,6 @@ if [ -z "$AUTH_PASS" ]; then
     AUTH_PASS=$(openssl rand -hex 8)
 fi
 
-read -s -p "请输入您的Hysteria密码（默认随机）: " HYSTERIA_PASS
-# 如果用户没有输入密码，则生成一个随机字符串
-if [ -z "$HYSTERIA_PASS" ]; then
-    HYSTERIA_PASS=$(openssl rand -hex 16)
-fi
-
 read -p "请输入xray端口（默认从10000-20000随机）: " PORT
 # 如果用户没有输入端口，则生成一个随机端口
 if [ -z "$PORT" ]; then
@@ -51,23 +44,11 @@ fi
 
 # 更新软件包列表并升级系统软件
 apt update
-apt install -y curl wget git debian-keyring debian-archive-keyring apt-transport-https crontabs nftables
+apt install -y curl wget git debian-keyring debian-archive-keyring apt-transport-https crontabs
 apt upgrade -y 
 
 # 运行apt自动清理
 apt autoremove -y
-
-# 设置系统缓冲区大小
-sysctl -w net.core.rmem_max=16777216
-sysctl -w net.core.wmem_max=16777216
-
-# 检查当前系统是否支持BBR
-grep -qF "tcp_bbr" /etc/modules || echo "tcp_bbr" >> /etc/modules
-grep -qF "net.core.default_qdisc=fq" /etc/sysctl.conf || echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-grep -qF "net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf || echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-
-# 应用修改
-sysctl -p
 
 # 下载 Go 二进制压缩包
 wget https://golang.org/dl/go$VERSION.linux-amd64.tar.gz
@@ -110,7 +91,6 @@ systemctl stop caddy.service
 cp /root/caddy /usr/bin
 
 # 修改Caddyfile
-
 cat <<EOF > /etc/caddy/Caddyfile
 :80 {
     root * /usr/share/caddy
@@ -132,89 +112,12 @@ cat <<EOF > /etc/caddy/Caddyfile
         }
     }
 }
-
-:8443 {
-         reverse_proxy https://www.coze.com { 
-            header_up Host {upstream_hostport}
-            header_up X-Forwarded-Host {host}
-        }
-}
 EOF
 
-#重启Caddy服务
+# 重启Caddy服务
 systemctl restart caddy.service
 
-# 安装Hysteria
-bash <(curl -fsSL https://get.hy2.sh/) 
-
-# 配置 nftables
-cat <<EOF > /etc/nftables.conf
-#!/usr/sbin/nft -f
-
-define INGRESS_INTERFACE = "eth0"
-define PORT_RANGE = 20000-21000
-define HYSTERIA_SERVER_PORT = 8443
-
-table inet hysteria_porthopping {
-  chain prerouting {
-    type nat hook prerouting priority dstnat; policy accept;
-    iifname $INGRESS_INTERFACE udp dport $PORT_RANGE counter redirect to :$HYSTERIA_SERVER_PORT
-  }
-}
-EOF
-
-# 启用 nftables 并重启服务
-systemctl enable nftables
-systemctl restart nftables
-
-# 修改Hysteria配置文件
-
-cat <<EOF > /etc/hysteria/config.yaml
-listen: 0.0.0.0:8443
-
-tls:
-  cert: /etc/hysteria/$DOMAIN.crt
-  key: /etc/hysteria/$DOMAIN.key
-
-auth:
-  type: password
-  password: $HYSTERIA_PASS
-
-quic:
-  initStreamReceiveWindow: 26843545 
-  maxStreamReceiveWindow: 26843545 
-  initConnReceiveWindow: 67108864 
-  maxConnReceiveWindow: 67108864
-
-acl:
-  inline: 
-    - reject(all, udp/443)
-    - reject(geoip:private)
-    - reject(geoip:cn)
-    - reject(geosite:cn)
-    - reject(geosite:category-ads-all)
-    - direct(all)
-
-masquerade:
-  type: proxy
-  proxy:
-    url: https://$DOMAIN/
-    rewriteHost: true
-  forceHTTPS: true
-EOF
-
-# 检查证书目录是否存在
-while [ ! -d "$CERT_DIR" ]; do
-    sleep 1
-done
-
-# 复制证书文件到/etc/hysteria
-cp -f "$CERT_DIR/$DOMAIN.crt" /etc/hysteria/
-cp -f "$CERT_DIR/$DOMAIN.key" /etc/hysteria/
-chmod 644 /etc/hysteria/*.crt
-chmod 644 /etc/hysteria/*.key
-
-# 安装xay
+# 安装Xray
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
 
 # 生成 x25519 密钥对并提取私钥和公钥
@@ -352,37 +255,13 @@ cat << EOF > /usr/local/etc/xray/config.json
 }
 EOF
 
-# 指定要添加的计划任务
-CRON_JOB="0 0 * * * systemctl restart hysteria-server"
-
-# 创建一个空的临时文件，如果不存在的话
-touch /tmp/crontab.tmp
-
-# 检查是否已经存在相同的任务
-if ! grep -qF "$CRON_JOB" /tmp/crontab.tmp; then
-    # 添加任务到临时文件
-    echo "$CRON_JOB" >> /tmp/crontab.tmp
-
-    # 导入更新后的 crontab
-    crontab /tmp/crontab.tmp
-fi
-
-# 删除临时文件
-rm /tmp/crontab.tmp
-
-# Hysteria服务开机自启
-systemctl enable hysteria-server.service
-
 # 重启服务
 systemctl restart xray.service
-systemctl restart hysteria-server.service
 
 echo 
 
-# 输出VLESS和Hysteria的连接信息
+# 输出VLESS连接信息
 echo "vless://$RANDOM_UUID@$SERVER_IP:$PORT?encryption=none&flow=xtls-rprx-vision&security=reality&sni=$DOMAIN&fp=chrome&pbk=$PUBLIC_KEY&sid=$RANDOM_SHORTID&type=tcp&headerType=none#xray-reality" > /root/vless_config.json
-
-echo "hysteria2://$HYSTERIA_PASS@$SERVER_IP:8443/?sni=$DOMAIN&alpn=h3&insecure=0#Hysteria2" > /root/hysteria_config.json
 
 echo "{\"listen\": \"socks://127.0.0.1:1080\",\"proxy\": \"https://$AUTH_USER:$AUTH_PASS@$DOMAIN\"}" > /root/naive.json
 
