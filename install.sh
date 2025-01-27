7d374777a9f6d2ee6a70c610dd07367b87746481
niu0506 <xxhhxx2015.11.11@gmail.com>
2025-01-27 15:42:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -114,6 +114,9 @@ tar -xvf caddy-forwardproxy-naive.tar.xz
 sudo cp /root/caddy-forwardproxy-naive/caddy /usr/bin
 # 为 /usr/bin 目录下的 caddy 文件添加执行权限
 sudo chmod +x /usr/bin/caddy
+# 删除下载的文件和解压后的目录
+rm -rf /root/caddy-forwardproxy-naive
+rm -f /root/caddy-forwardproxy-naive.tar.xz
 
 # 生成Caddy配置
 echo -e "\n${GREEN}生成Caddy配置文件...${NC}"
